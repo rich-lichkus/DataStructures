ae255f68f8e9c2bd11cf626e22bef744e6ad17b2
Richard Lichkus <rpl5018@gmail.com>
2014-07-28 09:28:08 -0700

    Initial Commit

---
+++ DataStructures/CKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CKAppDelegate.h
+//  DataStructures
+//
+//  Created by Richard Lichkus on 7/28/14.
+//  Copyright (c) 2014 Richard Lichkus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DataStructuresTests/DataStructuresTests.m
@@ -0,0 +1,34 @@
+//
+//  DataStructuresTests.m
+//  DataStructuresTests
+//
+//  Created by Richard Lichkus on 7/28/14.
+//  Copyright (c) 2014 Richard Lichkus. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DataStructuresTests : XCTestCase
+
+@end
+
+@implementation DataStructuresTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
