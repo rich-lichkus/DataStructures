8cd6dd7719dfedbf4c1f31391bb147fbee0a66f3
Richard Lichkus <rpl5018@gmail.com>
2014-08-20 20:41:35 -0700

    Initial tree structure

---
+++ DataStructures/Data Structures/CKTree.h
@@ -0,0 +1,13 @@
+//
+//  CKTree.h
+//  DataStructures
+//
+//  Created by Richard Lichkus on 8/20/14.
+//  Copyright (c) 2014 Richard Lichkus. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CKTree : NSObject
+
+@end
---
+++ DataStructures/Data Structures/CKTree.m
@@ -0,0 +1,17 @@
+//
+//  CKTree.m
+//  DataStructures
+//
+//  Created by Richard Lichkus on 8/20/14.
+//  Copyright (c) 2014 Richard Lichkus. All rights reserved.
+//
+
+#import "CKTree.h"
+
+@interface CKTree()
+
+@end
+
+@implementation CKTree
+
+@end
